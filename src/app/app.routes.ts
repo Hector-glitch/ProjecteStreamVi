e8bda662175f6cc6cacd47252c81168ae8bb4c0f
paupc <p.pulido.c@institutvidreres.cat>
2024-11-11 08:59:58 +0100

    Create second client (it works)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LlistaVideosComponent} from './llista-videos/llista-videos.component';
-import { ComprovarC2Component} from './comprovar-c2/comprovar-c2.component';
-
-
+import { LlistaVideosComponent } from './llista-videos/llista-videos.component';
+import { ComprovarC2Component } from './comprovar-c2/comprovar-c2.component';
 
 export const routes: Routes = [
-  { path: '', component: LlistaVideosComponent },
-  { path: 'client2', component: ComprovarC2Component },
+  { path: '', redirectTo: '/lista-videos', pathMatch: 'full' },
+  { path: 'lista-videos', component: LlistaVideosComponent },
+  { path: 'client2', component: ComprovarC2Component }
 ];
 
 @NgModule({
