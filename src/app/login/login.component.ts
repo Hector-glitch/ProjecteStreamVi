38c8ff3161f13abcf0539868206a9e980a4bd227
Héctor <hector.vinas@institutvidreres.cat>
2025-03-12 14:13:44 +0100

    Es detecten els usuaris premium i no premium i la sessio es mante iniciada al canviar de pagina

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import { getApp } from "firebase/app";
-import {NgIf} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import { NgIf } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    NgIf,
-    FormsModule
-  ],
+  imports: [NgIf, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   formData = {
     userEmail: '',
     userPassword: ''
@@ -24,24 +21,36 @@ export class LoginComponent {
 
   constructor(private router: Router) { }
 
+  ngOnInit() {
+    const sessionActive = localStorage.getItem('sessionActive');
+    if (!sessionActive) {
+      this.router.navigate(['/lista-videos']); // Torna a la pàgina de verificació
+    }
+  }
+
   async onSubmit() {
     try {
-      // Obté la instància de Firestore a partir de la app inicialitzada
       const db = getFirestore(getApp());
 
-      // Consulta la col·lecció "usuaris" filtrant per email i contrasenya
+      // Consulta a Firestore per verificar l'usuari
       const userQuery = query(
-          collection(db, "usuaris"),
-          where("email", "==", this.formData.userEmail),
-          where("contrasenya", "==", this.formData.userPassword)
+        collection(db, "usuaris"),
+        where("email", "==", this.formData.userEmail),
+        where("contrasenya", "==", this.formData.userPassword)
       );
 
       const querySnapshot = await getDocs(userQuery);
 
       if (!querySnapshot.empty) {
-        // Si l'usuari existeix, marquem la sessió com iniciada (aquí s'utilitza localStorage per simplicitat)
+        // L'usuari existeix
+        const userDoc = querySnapshot.docs[0]; // Agafa el primer document trobat
+        const userData = userDoc.data();
+
+        // Guarda la sessió a localStorage
         localStorage.setItem('isLogged', 'true');
-        // Redirigeix a la pàgina principal (llista-videos)
+        localStorage.setItem('isPremium', userData['isPremium'] ? 'true' : 'false');
+
+        // Redirigir a la pàgina de vídeos
         this.router.navigate(['/lista-videos']);
       } else {
         this.error = "Usuari no trobat o contrasenya incorrecta.";
